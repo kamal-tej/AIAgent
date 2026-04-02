6c19b686d4c3952d25b4673bc3e7d37248af2b01
kamal-tej <kamalkushiguthikonda@gmail.com>
2026-04-02 21:04:02 +0530

    added dynamic url to fetch from github secrets

--- frontend-ws/vite.config.ts
+++ frontend-ws/vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        // target: 'http://localhost:3001',
+        target: 'http://13.60.28.235:3001',
         changeOrigin: true,
       }
     }
